d772da9caf2945e8a5571f12608d3b14ced35505
Dmitry_E <ef_dim@mail.ru>
2024-06-14 21:53:32 +0500

    optimization of calculate_average_age functions

--- var7.py
+++ var7.py
@@ -7,49 +7,26 @@ import streamlit as st
 # Функция, которая вычисляет средний возраст пассажиров
 # каждого класса на основе данных из файла
 def calculate_average_age(lines, consider_family_size=False, family_size=None):
-    class1_total_age = 0
-    class1_passenger_count = 0
-    class2_total_age = 0
-    class2_passenger_count = 0
-    class3_total_age = 0
-    class3_passenger_count = 0
+    ages = {1: [], 2: [], 3: []}
 
     for line in lines:
         data = line.strip().split(',')
         try:
             age = float(data[6])
-            pclass = data[2]
-            sibsp = data[7]
+            pclass = int(data[2])
+            sibsp = int(data[7])
         except (ValueError, IndexError):
             continue
 
-        if (
-            not consider_family_size or
-            (consider_family_size and sibsp == str(family_size))
-        ):
-            if pclass == '1':
-                class1_total_age += age
-                class1_passenger_count += 1
-            elif pclass == '2':
-                class2_total_age += age
-                class2_passenger_count += 1
-            elif pclass == '3':
-                class3_total_age += age
-                class3_passenger_count += 1
+        if not consider_family_size or sibsp == family_size:
+            ages[pclass].append(age)
 
-    class1_average_age = 0
-    if class1_passenger_count > 0:
-        class1_average_age = class1_total_age / class1_passenger_count
+    averages = {
+        pclass: (sum(age_list) / len(age_list)) if age_list else 0
+        for pclass, age_list in ages.items()
+    }
 
-    class2_average_age = 0
-    if class2_passenger_count > 0:
-        class2_average_age = class2_total_age / class2_passenger_count
-
-    class3_average_age = 0
-    if class3_passenger_count > 0:
-        class3_average_age = class3_total_age / class3_passenger_count
-
-    return class1_average_age, class2_average_age, class3_average_age
+    return averages[1], averages[2], averages[3]
 
 
 def var7_main():
@@ -57,11 +34,11 @@ def var7_main():
         lines = file.readlines()
 
     # Визуализируем решение
-    st.subheader(
-        'Средний возраст пассажиров парохода «Титаник» '
+    st.write(
+        '**Средний возраст пассажиров парохода «Титаник» '
         'по каждому классу обслуживания в зависимости '
         'от количества родственников (братьев, сестер, '
-        'сводных братьев, сводных сестер, супругов на борту).'
+        'сводных братьев, сводных сестер, супругов на борту).**'
     )
     class_service = ['1 класс', '2 класс', '3 класс']
     selected_family_size = st.number_input(
@@ -73,30 +50,33 @@ def var7_main():
     # в зависимости от выбора family_size_options
     if family_size_options:
         class1_avg, class2_avg, class3_avg = calculate_average_age(lines)
-        st.table({'Класс обслуживания': class_service,
-                  'Средний возраст': [class1_avg, class2_avg, class3_avg]})
-        plt.figure(figsize=(8, 4))
+        st.dataframe({'Класс обслуживания': class_service,
+                      'Средний возраст': [class1_avg, class2_avg, class3_avg]})
+        plt.figure(figsize=(10, 3))
         plt.bar(class_service, [class1_avg, class2_avg, class3_avg])
-        plt.xlabel('Класс пассажира (Pclass)')
         plt.ylabel('Средний возраст')
-        plt.title('Средний возраст пассажиров по классам обслуживания')
+        plt.title('Средний возраст пассажиров по классам обслуживания',
+                  fontsize=12)
         st.pyplot(plt)
     else:
         if selected_family_size == 'Без учета родственников':
             class1_avg, class2_avg, class3_avg = calculate_average_age(lines)
-            st.table({'Класс обслуживания': class_service,
-                      'Средний возраст': [class1_avg, class2_avg, class3_avg]})
+            st.dataframe({'Класс обслуживания': class_service,
+                          'Средний возраст': [class1_avg,
+                                              class2_avg,
+                                              class3_avg]})
         else:
             family_size = int(selected_family_size)
             class1_avg, class2_avg, class3_avg = calculate_average_age(
                 lines, consider_family_size=True, family_size=family_size
                 )
-            st.table({'Класс обслуживания': class_service,
-                      'Средний возраст': [class1_avg, class2_avg, class3_avg]})
+            st.dataframe({'Класс обслуживания': class_service,
+                          'Средний возраст': [class1_avg,
+                                              class2_avg,
+                                              class3_avg]})
 
-        plt.figure(figsize=(8, 4))
+        plt.figure(figsize=(10, 3))
         plt.bar(class_service, [class1_avg, class2_avg, class3_avg])
-        plt.xlabel('Класс пассажира (Pclass)')
         plt.ylabel('Средний возраст')
         if selected_family_size == '1':
             plt.title(
